a6f15a213fb75130e8890496029c497ba6ad7514
Remark Ekz <yulianlatypov@yandex.ru>
2022-08-03 01:32:48 +0500

    Финальный спринт первое ревью 500 наладил

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 
 
@@ -5,9 +6,13 @@ def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
 
+def server_error(request):
+    return render(request, 'core/500.html', status=500)
+
+
+def permission_denied(request, exception):
+    return render(request, 'core/403.html', status=403)
+
+
 def csrf_failure(request, reason=''):
-    return render(request, 'core/403.html', {'path': request.path}, status=403)
-
-
-def page_internal_server(request, reason=''):
-    return render(request, 'core/500.html', {'path': request.path}, status=500)
+    return render(request, 'core/403csrf.html')
